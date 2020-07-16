c392ec9198ed0e6eee19af15db2ca166d8af853d
wiedarthapratama <wiedarthapratama@gmail.com>
2020-07-16 12:14:43 +0700

    6. Allowing Users to Log In

--- lib/providers/auth_provider.dart
+++ lib/providers/auth_provider.dart
@@ -7,12 +7,21 @@ class AuthProvider with ChangeNotifier {
   DateTime _expireDate;
   String _userId;
 
-  Future<void> signup(String email, String password) async {
-    const url =
-        "https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=AIzaSyD0ofo-SG8AVOZxPHTP_HLL-vIY0h4MDAY";
+  Future<void> _authenticate(
+      String email, String password, String urlSegment) async {
+    final url =
+        "https://identitytoolkit.googleapis.com/v1/accounts:$urlSegment?key=AIzaSyD0ofo-SG8AVOZxPHTP_HLL-vIY0h4MDAY";
     final response = await http.post(url,
         body: json.encode(
             {'email': email, 'password': password, 'returnSecureToken': true}));
     print(json.decode(response.body));
+  }
+
+  Future<void> signup(String email, String password) async {
+    return _authenticate(email, password, 'signUp');
+  }
+
+  Future<void> login(String email, String password) async {
+    return _authenticate(email, password, 'signInWithPassword');
   }
 }
